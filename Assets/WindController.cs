a06292bd9432520e4f822ee4c5b91fd1cc9721d4
Yakov Shcherba <sherbayv@gmail.com>
2018-09-20 21:07:50 +0600

    added wind zone with trails for particles

---
+++ Assets/WindController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindController : MonoBehaviour {
+
+	public float windSpeed = 5.0f;
+
+	private GameObject _windZone;
+	private Mesh _windZoneMesh;
+	private ParticleSystem _windParticles;
+
+	void Awake() {
+		_windZone = gameObject;
+		_windParticles = gameObject.GetComponent<ParticleSystem>();
+		_windZoneMesh = gameObject.GetComponent<MeshFilter>().mesh;
+
+	}
+
+	void HideParticles() {
+		_windParticles.Stop();
+	}
+
+	void UpdateParticlesSpeed(float newSpeed) {
+		ParticleSystem.MainModule updatableMain = _windParticles.main;
+		if (newSpeed == updatableMain.startSpeed.constant) {
+			return;
+		}
+
+		ParticleSystem.NoiseModule updatableNoise = _windParticles.noise;
+		updatableNoise.frequency = windSpeed / 25.0f;
+		updatableMain.startSpeed = windSpeed;
+	}
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		UpdateParticlesSpeed(windSpeed);
+		_windZoneMesh.RecalculateBounds();
+		Debug.Log("_windZoneMesh.bounds = " + _windZoneMesh.bounds);
+	}
+
+}
